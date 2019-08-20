9d80ca12620ed08d38cf2124b2b0acf7b4157aab
Richard Barnes <rbarnes@umn.edu>
2019-08-20 10:04:06 -0700

    Added more debugging info using a technique from @pradeep

--- cpp_interface.cpp
+++ cpp_interface.cpp
@@ -1,17 +1,32 @@
 #include <pybind11/pybind11.h>
 #include <arrayfire.h>
+#include <af/macros.h>
 #include <cmath>
 #include <iostream>
+
+uint64_t get_gpu_pointer(af::array &arr){
+    const auto adr = reinterpret_cast<uint64_t>(arr.device<void>());
+    arr.unlock();
+    return adr;
+}
 
 uint64_t testfunc(
     uint64_t input_ptr,
     uint64_t input2_ptr
 ){
+    std::cout<<"\n\n###############C++"<<std::endl;
+    // AF_MEM_INFO("before test");
+    af_print_mem_info("before test", 0);
+
     af::array input (*(af_array**)input_ptr);
     af::array input2(*(af_array**)input2_ptr);
 
+    std::cout<<"input_ptr="<<std::hex<<get_gpu_pointer(input)<<std::endl;
+    std::cout<<"input2_ptr="<<std::hex<<get_gpu_pointer(input2)<<std::endl;
+
     af::array out = input*input2;
 
+    af_print_mem_info("after test", 0);
     //Need to return device pointer since otherwise af_array reaches end of
     //life, likely before it can be saved by getting reinstantiated
     return reinterpret_cast<uint64_t>(out.device<float>());
--- run.py
+++ run.py
@@ -4,10 +4,14 @@ import numpy as np
 import ctypes
 import testlib
 
+def get_gpu_pointer(x):
+    adr = af.device.get_device_ptr(x)
+    af.device.unlock_array(x)
+    return adr.value
+
 # use gpu backend
 af.set_backend('cuda')
 af.set_device(0)  # select the gpu to use
-
 af.info()
 
 van = np.array([1, 2, 3, 5])
@@ -21,8 +25,18 @@ afvan = afvan.as_type(af.Dtype.f32)
 afthr = af.interop.from_ndarray(two)
 afthr = afthr.as_type(af.Dtype.f32)
 
-print("Loop", flush=True)
+af.device.print_mem_info("before loops")
+
+print("afvan", hex(get_gpu_pointer(afvan)))
+print("afthr", hex(get_gpu_pointer(afthr)))
+
+print("\n\nLoop", flush=True)
 for x in range(10):
+    af.device.print_mem_info("top of the loop")
     b = testlib.testfunc(ctypes.addressof(afvan.arr), ctypes.addressof(afthr.arr))
+    print("\n\n################Python", flush=True)
+    af.device.print_mem_info("back in python")
     c = af.array.Array(src=b, dims=afvan.shape, dtype=af.Dtype.f32, is_device=True)
+    af.device.print_mem_info("after defining array")
     af.device.lock_array(c) #TODO: Do I need to do this to take ownership of the memory?
+    af.device.print_mem_info("after locking array")
