63cba1b899776767a26a7ee494ae3964caa6dab4
Richard Barnes <rbarnes@umn.edu>
2019-08-20 22:34:14 -0700

    Clarify what works and doesn't

--- Makefile
+++ Makefile
@@ -1,14 +1,5 @@
-#[1/2] /usr/bin/c++  -Dpathai_EXPORTS -I../submodules/pybind11/include -I/usr/include/python3.6m -fPIC -fvisibility=hidden   -std=c++14 -flto -fno-fat-lto-objects -std=gnu++14 -MD -MT src/CMakeFiles/pathai.dir/442.cpp.o -MF src/CMakeFiles/pathai.dir/442.cpp.o.d -o src/CMakeFiles/pathai.dir/442.cpp.o -c ../src/442.cpp
-
-#[2/2] : && /usr/bin/c++ -fPIC    -shared  -o src/pathai.cpython-36m-x86_64-linux-gnu.so src/CMakeFiles/pathai.dir/442.cpp.o  -flto -lafcuda && :
-
-all: cpp_interface.o
-	g++-6 -g -o testlib.so -shared -fPIC cpp_interface.o -flto -lafcuda -lcudart -Wall -Wextra
-
-cpp_interface.o: cpp_interface.cpp
-	g++-6 -g -fPIC -c cpp_interface.cpp -I/usr/include/python3.6m -Isubmodules/pybind11/include -lafcuda -laf -Wall -Wextra -fvisibility=hidden
+all:
+	python3 setup.py build_ext --inplace
 
 clean:
-	rm -rf *.o *.so
-
-# g++-6 -o pathai.so -O3 -Wall -shared -std=c++14 -fPIC 
+	rm -rf build/ test.cpp test.cpython-36m-x86_64-linux-gnu.so
--- cpp_interface.cpp
+++ cpp_interface.cpp
@@ -4,31 +4,31 @@
 #include <cmath>
 #include <iostream>
 
-long long testfunc(
-    long long input_ptr
-){
+long long testfunc(long long input_ptr){
     std::cout<<"\n\n###############C++"<<std::endl;
     // AF_MEM_INFO("before test");
     af_print_mem_info("before test", 0);
 
-    af_array in = *reinterpret_cast<af_array**>(input_ptr);
-    af_array temp;
-    af_copy_array (&temp, in);
+    //Works with these lines
+    {
+        af_array in = *reinterpret_cast<af_array**>(input_ptr);
+        af_array temp;
 
-    af::array input(temp);
+        af_copy_array (&temp, in);
+        af::array input(temp);
 
-    std::cout<<"Size = "<<input.dims(0)<<std::endl;
+        std::cout<<"Size = "<<input.dims(0)<<std::endl;
+    }
+
+    //Breaks with these lines
+    // {
+    //     af_array in = *reinterpret_cast<af_array**>(input_ptr);
+    //     af::array input(in);
+
+    //     std::cout<<"Size = "<<input.dims(0)<<std::endl;
+    // }
+
 
     af_print_mem_info("after test", 0);
     return 0;
 }
-
-// PYBIND11_MODULE(testlib, m) {
-//     m.doc() = "core functions";
-
-//     af::setBackend(AF_BACKEND_CUDA);
-//     af::setDevice(0);
-//     af_info();
-
-//     m.def("testfunc", &testfunc, "TODO desc");
-// }
